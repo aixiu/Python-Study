519c21922601db672d3a11b8e1b149b04fe918dd
aixiu <4815563@qq.com>
2020-05-06 00:50:17 +0800

    分支和循环

---
+++ 03-分支结构.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' 
+用户身份验证案例
+'''
+
+# user = input('请输入用户名： ')
+# psd = input('请输入密码： ')
+
+# if user == 'admin' and psd == '123456':
+#     print('身份验证成功！')
+# else:
+#     print('哪来的回哪去！')
+
+
+
+'''
+For 循环实现1~100求和
+'''
+
+sum = 0
+for i in range(101):
+    sum += i
+
+print(sum)
