cfb913ed99a3c5311771934afd8b265b7400a38f
aixiu <4815563@qq.com>
2020-05-08 11:48:29 +0800

    新的知识点

--- Study01-15/03-分支结构.py
+++ Study01-15/03-分支结构.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-''' 
-用户身份验证案例
-'''
+print("开始运行系统")
 
-# user = input('请输入用户名： ')
-# psd = input('请输入密码： ')
-
-# if user == 'admin' and psd == '123456':
-#     print('身份验证成功！')
-# else:
-#     print('哪来的回哪去！')
-
-
-
-'''
-For 循环实现1~100求和
-'''
-
-sum = 0
-for i in range(101):
-    sum += i
-
-print(sum)
+while True:
+    user = input("请输入用户名：")
+    pwd =  input("请输入密码：")
+    if user == 'wupeiqi' and pwd == "oldboy":
+        print("登录成功")
+        break
+    else:
+        print("用户名或密码错误，请重新登录")
+print("系统结束")
