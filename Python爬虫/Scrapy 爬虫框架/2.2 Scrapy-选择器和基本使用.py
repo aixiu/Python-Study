232f4b75194a8021bd154f36c1bb37f9fbc23f38
aixiu <4815563@qq.com>
2021-01-30 20:10:57 +0800

    scrapy框架开始

--- Python爬虫/Scrapy 爬虫框架/2.2 Scrapy-选择器和基本使用.py
+++ Python爬虫/Scrapy 爬虫框架/2.2 Scrapy-选择器和基本使用.py
@@ -6,8 +6,8 @@
 XPath 是一门在xml文档中查找信息的语言，它可以在XML文档中对于原色和属性进行遍历。其内置了超过100个内建函数，这些函数用于对字符串值，数值、日期、时间进行比较遍历。总之是一门很方便的语言。如过要具体学习请看
 菜鸟教程-Xpath：XPath 教程 | 菜鸟教程  https://www.runoob.com/xpath/xpath-tutorial.html
 在网络爬虫中，我们只需要利用XPath来采集数据，所以只要掌握一些基本语法，就可以上手使用了。
-
-
 '''
+
+
 
 # 本文链接：https://zhuanlan.zhihu.com/p/26854842
