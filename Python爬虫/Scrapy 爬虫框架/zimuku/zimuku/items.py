76a6930f3827b7e8476d5f9bbfc81d2eb5f2594b
aixiu <4815563@qq.com>
2021-01-31 19:34:12 +0800

    zimuku爬虫练习

---
+++ Python爬虫/Scrapy 爬虫框架/zimuku/zimuku/items.py
@@ -0,0 +1,12 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ZimukuItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    subname = scrapy.Field() #字母的名字
---
+++ Python爬虫/Scrapy 爬虫框架/zimuku/zimuku/pipelines.py
@@ -0,0 +1,18 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class ZimukuPipeline:
+    def process_item(self, item, spider):
+        
+        # 只要求简单的话，
+        # 我们把爬到的结果打印一下吧
+        print(item)
+
+        return item
