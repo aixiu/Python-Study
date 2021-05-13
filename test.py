4f6b9fdc754cd8b82ed66df4b33b4eb62304f853
aixiu <4815563@qq.com>
2021-05-13 17:54:27 +0800

    日常

--- test.py
+++ test.py
@@ -5,8 +5,16 @@ import requests
 
 session = requests.session()
 
-url = 'http://www.baiu.com'
+# url = 'https://www.douban.com/search?q=python&cat=1001'
 
-res = session.get(url)
+# res = session.get(url)
 
-print(res)
+# print(res)
+
+# r = requests.get(url, params={'q': 'python', 'cat': '1001'})
+# print(r.url)
+
+r = requests.get('https://www.douban.com/search?q=python&cat=1001&format=json')
+print(r.json)
+
+
