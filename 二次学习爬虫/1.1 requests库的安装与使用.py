bf0bf24e4f5f96f1ebbf66922d12c4e2f2742905
aixiu <4815563@qq.com>
2022-03-23 15:42:58 +0800

    更新学习笔记

--- 二次学习爬虫/1.1 requests库的安装与使用.py
+++ 二次学习爬虫/1.1 requests库的安装与使用.py
@@ -2,8 +2,21 @@
 # -*- coding: utf-8 -*-
 #@Author : Aixiu
 
+
+# https://zhuanlan.zhihu.com/p/26681429
+# requests 安装 pip install requests
+
+
+# requests抓取网页的通用框架
 import requests
 
-r = requests.get('http://www.baidu.com')
-
-print(r.text)
+def getHtmlText(url):
+    try:
+        r = requests.get(url, timeout=30)
+        # 如果状态码不是200 则应发HTTOError异常
+        r.raise_for_status()
+        # 设置正确的编码方式
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return 'Something Wrong!'
---
+++ 二次学习爬虫/1.2 BS4库的安装与使用.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#@Author : Aixiu
+
+# bs4库的安装  pip install beautifulsoup4
